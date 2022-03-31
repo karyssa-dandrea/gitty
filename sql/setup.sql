34cf91f1f8789edcc27de2950e9266be813c2e64
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-31 11:33:13 -0700

    created sql tables for user and posts

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT,
+    avatar TEXT
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    post VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
